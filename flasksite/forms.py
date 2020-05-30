2e6da470e05126cb3124b1d32ffd4995a8e5bbb8
Peter <peter.a.sunga@gmail.com>
2020-05-30 22:44:53 +0900

    profile

--- flasksite/forms.py
+++ flasksite/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flasksite.models import User
@@ -54,3 +55,44 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+
+    first_name = StringField('First Name',
+                            validators=[DataRequired()])
+
+    last_name = StringField('Last Name',
+                            validators=[DataRequired()])
+
+    postal_code = StringField('Postal Code',
+                            validators=[DataRequired()])
+
+    my_number = StringField('My Number',
+                            validators=[Length(min=1, max=12)])
+
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is already taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:   
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email is already taken. Please choose a different one.')
+
+
+    def validate_mynumber(self, email):   
+        if my_number.data != current_user.my_number:  
+            my_number = User.query.filter_by(my_number=my_number.data).first()
+            if my_number:
+                raise ValidationError('That My Number is already taken. Please choose a different one.')
