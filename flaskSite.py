8ef12e02e5d7c677585df8df4ac61cd68fd6725b
Peter <peter.a.sunga@gmail.com>
2020-05-30 15:10:46 +0900

    update flaskSite

--- flaskSite.py
+++ flaskSite.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+posts =[
+    {
+        'author': 'Corey Schafer',
+        'title': 'blog post 1',
+        'content': "dumb",
+        'date_posted': 'May 1st, 2019'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'blog post 2',
+        'content': "dumber",
+        'date_posted': 'April 20th, 2020'
+    }
+]
 
-def hello():
-    return "<h1>Homepage</h1>"
+
+@app.route('/')
+@app.route('/home')
+def homepage():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="about")
 
 
 if __name__ == "__main__":
